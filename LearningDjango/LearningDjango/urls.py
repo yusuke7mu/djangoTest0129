df97ec81d86913e6d368c0ddbda37311c113a0fd
higuchi <higuchi@NUS_MBP002>
2019-02-09 16:31:25 +0900

    最初の Django アプリの作成

--- LearningDjango/LearningDjango/urls.py
+++ LearningDjango/LearningDjango/urls.py
@@ -3,6 +3,7 @@ Definition of urls for LearningDjango.
 """
 
 from django.conf.urls import include, url
+import HelloDjangoApp.views
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -18,4 +19,6 @@ urlpatterns = [
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+     url(r'^$', HelloDjangoApp.views.index, name='index'),
+     url(r'^home$', HelloDjangoApp.views.index, name='home'),
 ]
